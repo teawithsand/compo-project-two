f31c11c371dd5fdfbe15fcca8baa1ce6e7f1bfd9
Kowalskiexe <kowalski.exe@gmail.com>
2022-11-19 18:54:41 +0100

    pfp

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -2,6 +2,9 @@ function ProfilePage() {
     return (
         <>
             <h1 style= {{"font-size":"50px","margin-top":"20px"}}>Mój profil</h1>
+            <div id="profile-frame">
+                <img id="profile-picture" src="images/sample_photo.jpg" />
+            </div>
         </>
     )
 }
