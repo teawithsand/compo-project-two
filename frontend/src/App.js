60605a42725955154788a4c7e7d41108207b9f5b
Kowalskiexe <kowalski.exe@gmail.com>
2022-11-19 15:09:07 +0100

    basic home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import './App.scss'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import { QuizPage } from './pages/QuizPage'
-import { Header } from './components/layout/Header'
+import HomePage from './pages/HomePage'
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <Header />
                 <Routes>
                     <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/quiz" element={<QuizPage />}></Route>
+                    <Route path="/form" element={<div />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
